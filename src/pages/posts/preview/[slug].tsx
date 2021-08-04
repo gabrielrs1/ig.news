d7b0c92ac03d81992271c6cda5ee330551180e96
Gabriel Ribeiro <gabriellribeiro208@gmail.com>
2021-08-04 10:14:36 -0300

    Preview posts

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,12 +1,15 @@
-import { GetServerSideProps } from "next"
-import { getSession } from "next-auth/client"
+import { GetStaticProps } from "next"
+import { useSession } from "next-auth/client"
 import Head from "next/head"
+import Link from "next/link"
+import router from "next/router"
 import { RichText } from "prismic-dom"
-import { getPrismicClient } from "../../services/prismic"
+import { useEffect } from "react"
+import { getPrismicClient } from "../../../services/prismic"
 
-import styles from "./post.module.scss"
+import styles from "../post.module.scss"
 
-interface PostProps {
+interface PostPreviewProps {
     post: {
         slug: string,
         title: string,
@@ -15,7 +18,15 @@ interface PostProps {
     }
 }
 
-export default function Post({ post }: PostProps) {
+export default function PostPreview({ post }: PostPreviewProps) {
+    const [session] = useSession()
+
+    useEffect(() => {
+        if(session?.activeSubscription) {
+            router.push(`/posts/${post.slug}`)
+        }
+    }, [session])
+
     return (
         <>
             <Head>
@@ -27,36 +38,40 @@ export default function Post({ post }: PostProps) {
                     <h1>{post.title}</h1>
                     <time>{post.updatedAt}</time>
                     <div
-                        className={styles.postContent}
+                        className={`${styles.postContent} ${styles.previewContent}`}
                         dangerouslySetInnerHTML={{__html: post.content}}
                     /> {/* essa propriedade dangerouslySetInnerHTML pode ser utitlizada somente em alguns casos, porque até mesmo o nome já diz. Nessa aplicação o prismic tem tratativa do conteúdo html para que não haja qualquer forma de manipulação por alguma pessoa indesejada */}
+                
+                    <div className={styles.continueReadings}>
+                        Wanna continue reading?
+                        <Link href="/">
+                            <a>Subscribe now 🤗</a>
+                        </Link>
+                    </div>
                 </article>
             </main>
         </>
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-    const session = await getSession({ req })
-    const { slug } = params
-
-    if(!session?.activeSubscription) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            }
-        }
+export const getStaticPaths = () => {
+    return {
+        paths: [],
+        fallback: 'blocking'
     }
+}
 
-    const prismic = getPrismicClient(req)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const { slug } = params;
+
+    const prismic = getPrismicClient()
 
     const response = await prismic.getByUID('post', String(slug), {})
 
     const post = {
         slug,
         title: RichText.asText(response.data.title),
-        content: RichText.asHtml(response.data.content),
+        content: RichText.asHtml(response.data.content.splice(0, 3)),
         updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
             day: '2-digit',
             month: 'long',
